7551993a8a190330f906f1ca57d6a7afbff3e4a8
dkhos17 <48618259+dkhos17@users.noreply.github.com>
2020-01-25 22:25:20 +0400

    contest

---
+++ contest/D.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define ins insert
+#define Pq priority_queue
+#define u_set unordered_set
+#define MOD 1000000007
+#define MAXN 100001
+#define set_val(arr,v) memset(arr, v, sizeof(arr))
+#define FOR(i,from,to) for(int i=from; i<to; i++)
+#define FOL(i,from,to) for(int i=from; i>=to; i--)
+#define B() begin()
+#define E() end()
+#define F first
+#define S second
+
+
+int main() {
+	ios::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
+	
+	return 0; 
+}
---
+++ contest/L.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mp make_pair
+#define pb push_back
+#define ins insert
+#define Pq priority_queue
+#define u_set unordered_set
+#define MOD 1000000007
+#define MAXN 100001
+#define set_val(arr,v) memset(arr, v, sizeof(arr))
+#define scan(n) scanf("%d", &n)
+#define scanP(x,y) scanf("%d %d", &x, &y)
+#define scanPL(x,y) scanf("%lld %lld", &x, &y)
+#define FOR(i,from,to) for (int i=from; i<to; i++)
+#define B() begin()
+#define E() end()
+#define F first
+#define S second
+
+
+int main() {
+	ios::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
+	
+	return 0; 
+}
