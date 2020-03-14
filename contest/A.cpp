6f141e77637d4bdf5ee868c88870c330db83f3fb
unknown <dkhos17@freeuni.edu.ge>
2020-03-14 21:00:41 +0400

    update ABCD

--- contest/A.cpp
+++ contest/A.cpp
@@ -4,21 +4,36 @@ using namespace std;
 #define mp make_pair
 #define pb push_back
 #define ins insert
-#define Pq priority_queue
-#define u_set unordered_set
 #define MOD 1e9+7
 #define MAXN 100001
+#define Pq priority_queue
+#define u_set unordered_set
 #define set_val(arr,v) memset(arr, v, sizeof(arr))
 #define FOR(i,from,to) for(int i=from; i<to; i++)
 #define FOL(i,from,to) for(int i=from; i>=to; i--)
+#define YES printf("YES\n")
+#define NO printf("NO\n")
 #define B() begin()
 #define E() end()
 #define F first
 #define S second
-//  int  __builtin_popcount(unsigned int)
+
+int solve() {
+	int n;
+	cin >> n;
+	vector<int> v(n);
+	for(auto& x : v) cin >> x;
+
+
+	return 0;
+}
 
 int main() {
 	ios::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
-
+	int t;
+	cin >> t;
+	while (t--) 
+		solve();
+	
 	return 0; 
 }
