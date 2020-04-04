db3e2efc290629f96308ced1916dc5454a03ab9b
dkhos17 <dkhos17@freeuni.edu.ge>
2020-04-04 19:29:43 +0400

    update

--- contest/A.cpp
+++ contest/A.cpp
@@ -24,7 +24,6 @@ int solve() {
 	vector<int> v(n);
 	for(auto& x : v) cin >> x;
 
-
 	return 0;
 }
 
@@ -32,7 +31,7 @@ int main() {
 	ios::sync_with_stdio(0);  cin.tie(0); cout.tie(0);
 	int t;
 	cin >> t;
-	while (t--) 
+	while(t--)
 		solve();
 	
 	return 0; 
